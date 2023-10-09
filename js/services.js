f215335b1ca3116354224e37fc1650a96f3debd0
manAs3345 <88757580+manAs3345@users.noreply.github.com>
2023-10-09 11:27:30 +0800

    Website Content

---
+++ js/services.js
@@ -0,0 +1,14 @@
+function handleAuditingModal(){
+    const servicesModal = document.getElementById('services-modal');
+    servicesModal.style.display = 'block';
+
+}
+function handleAccountingModal(){
+    const servicesModal = document.getElementById('services-modal');
+    servicesModal.style.display = 'block';
+
+}
+function closeModal(){
+    const servicesModal = document.getElementById('services-modal');
+    servicesModal.style.display = 'none';
+}
